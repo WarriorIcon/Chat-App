1e8f53ddc22f4c3b68ebf5880d2b12bf06e5bab2
Scott Cyrus Jafary <scottcyrusj@gmail.com>
2021-05-16 18:29:58 +0700

    Chat pt 1

--- public/main.js
+++ public/main.js
@@ -1,1 +1,31 @@
 const socket = io();
+
+// Message from server
+socket.on('message', message => {
+    outputMessage(message)
+    console.log(message)
+})
+
+//message submit
+const chatForm = document.getElementById('chat-form')
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // get client message text
+    const msg = e.target.elements.msg.value;
+
+    // emit message to the server
+    socket.emit('chatMessage', msg);
+})
+
+//output message to DOM
+
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const path = require('path')
+
 const http = require('http');
 const socketio = require('socket.io')
-
 
 const app = express();
 const server = http.createServer(app)
 const io = socketio(server);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-//do this  when a client connects
+//when a client connects to the server we...
 io.on('connection', socket => {
-    console.log('New websocket connection')
+    
+    //welcome the new user client-side
+    socket.emit('message', "Welcome to Chatcord");
+
+    //broadcast to all users in the room about when new clients connect. 
+    socket.broadcast.emit('message', 'A user has joined the chat!')
+
+    // run when a client disconnects from server
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat!');
+    })
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg)
+    })
 })
+
+
+
 
 const PORT = 3000 || process.env.port;
 
