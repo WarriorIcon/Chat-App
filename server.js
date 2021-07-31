f91d09d84f025b7f0b4be97ac145c39c81eb2a9c
Scott Cyrus Jafary <scottcyrusj@gmail.com>
2021-07-31 14:32:39 +0700

    port fix again

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ io.on('connection', socket => {
 
 
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${port}!`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}!`))
 
